9143169737d21ee877545527afeafce0918b888a
EHronek <emmanuelhronek@gmail.com>
2023-10-05 21:24:10 +0000

    hello world projects

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+  * main - first execution point
+  * Return - always return zero to mean success
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
