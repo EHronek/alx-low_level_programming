63c45813e07ac30cac42413c2a9e35967ecd34be
EHronek <emmanuelhronek@gmail.com>
2023-10-06 20:16:54 +0000

    advanced 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - Entry point for code
+  * Return: 0 (success)
+  */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '8'; a++)
+	{
+		for (b = '1'; b <= '9'; b++)
+		{
+			putchar(a);
+			putchar(b);
+			if (b > a)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
+	return (0);
+}
