3031ee8223ba2bc86ad52057d1e0437952b428fe
EHronek <emmanuelhronek@gmail.com>
2023-10-11 20:53:10 +0000

    function that prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * print_alphabet - print alphabet in lowercase
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
