a32bd882b9c6d0b9b45ed2ddeb6bfbda28dc2c22
EHronek <emmanuelhronek@gmail.com>
2024-01-13 02:59:19 +0000

    pushing task 8

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,6 @@
 /**
   * dlistint_len - counts the number of eleements in a doubly linked list
   * @h: the head of the dlistint_t list
-  * 
   * Return: the number of elements in the dlistint_t list
   */
 size_t dlistint_len(const dlistint_t *h)
